37bfe6276f285195eecea47b9bda48086b163b22
liyunfeng <youyunpyy@163.com>
2021-05-12 20:14:02 +0800

    添加client\server

---
+++ src/main/java/com/xuzhu/client/NettyClient.java
@@ -0,0 +1,9 @@
+package com.xuzhu.client;
+
+/**
+ * @author yunfengli
+ * @date 2021/5/12
+ * @desc
+ */
+public class NettyClient {
+}
---
+++ src/main/java/com/xuzhu/server/NettyServer.java
@@ -0,0 +1,9 @@
+package com.xuzhu.server;
+
+/**
+ * @author yunfengli
+ * @date 2021/5/12
+ * @desc
+ */
+public class NettyServer {
+}
